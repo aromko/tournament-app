ba13e53edc5810626c929ff6b91b1cf74666f388
aromko <aromkoapp@gmail.com>
2025-08-19 16:00:01 +0200

    update test workflow: add lcov reporter for coverage and specify lcov info file in Codecov configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     css: true,
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html'],
+      reporter: ['text', 'html', 'lcov'],
       all: true,
       thresholds: {
         statements: 75,
